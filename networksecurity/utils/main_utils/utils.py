3694781983675d77234e4bc6a994caf14bab8b4a
somen2003 <somenmishra333@gmail.com>
2025-08-10 13:44:13 +0530

    Pipeline complete

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -89,7 +89,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            #model.fit(X_train, y_train)  # Train model
+           # Train model
 
             y_train_pred = model.predict(X_train)
 
